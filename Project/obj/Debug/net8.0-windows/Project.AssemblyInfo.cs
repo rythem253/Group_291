842a1884bcb887381474191c062a3f85e741f53a
rythem253 <miolavoro9@gmail.com>
2026-06-09 14:17:27 -0600

    wip
    wip

--- Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b3ced34ceb98a43b30b2a37319c13cbf20ce1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c939a9daa1affebc38e2f3bb66ea6ea822cda909")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
